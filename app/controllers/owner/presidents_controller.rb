f4a8889b2521eaf84fc4e7a35c50ffc9f9a887ef
valerian barreau <valerianextra@gmail.com>
2023-11-28 18:13:20 +0300

    create a president

--- app/controllers/owner/presidents_controller.rb
+++ app/controllers/owner/presidents_controller.rb
@@ -1,7 +1,20 @@
 class Owner::PresidentsController < ApplicationController
   def new
+    @president = President.new
   end
 
   def create
+    @president = President.new(president_params)
+    @president.user = current_user
+    if @president.save
+      redirect_to owner_president_path(@president)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  
+  def president_params
+    params.require(:president).permit(:name, :description)
   end
 end
