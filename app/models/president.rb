2a5e24de68d17e465a2e12f70c533bca7422c4a9
Deborah Parent <deborah.morchipont@hotmail.fr>
2023-11-28 15:49:17 +0100

    index qui fonctionne

--- app/models/president.rb
+++ app/models/president.rb
@@ -1,5 +1,5 @@
 class President < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :first_name, :last_name, :description, presence: true
+  validates :name, :description, presence: true
 end
