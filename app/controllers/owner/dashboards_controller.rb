d088486008952676bec0ff8fd0e0c32d9dbd180f
kiwismoothie <lauraberretta@hotmail.com>
2023-11-29 15:43:24 +0100

    dashboard debo MAJ, Valerian map, kevin visuels index dynamiques

--- app/controllers/owner/dashboards_controller.rb
+++ app/controllers/owner/dashboards_controller.rb
@@ -2,9 +2,6 @@ class Owner::DashboardsController < ApplicationController
   def show
     @my_presidents = President.where(user: current_user)
     @my_bookings = Booking.where(user: current_user)
-    # @bookings_of_my_presidents = Booking.where
+    @bookings_of_my_presidents = Booking.includes(:presidents).where(presidents: { user: current_user })
   end
 end
-# une div pour mes réservations que j'ai faites
-# une div pour les réservations de mes présidents
-# une div pour mes présidents
