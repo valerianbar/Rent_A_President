7c2e16ff22b1e65e9e7fb0af5767300a5145efb3
kiwismoothie <lauraberretta@hotmail.com>
2023-11-30 14:36:55 +0100

    resversation finie

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class BookingsController < ApplicationController
   def create
     string_dates = params[:booking][:start_date].split(" to ")
@@ -5,10 +7,9 @@ class BookingsController < ApplicationController
     end_date = Date.parse(string_dates[1])
     @booking = Booking.new(start_date: start_date, end_date: end_date)
     @booking.user = current_user
-    @booking.president = President.find(params[:president_id])
-    raise
+    @booking.president = President.find(params[:booking][:president_id])
     if @booking.save
-      redirect_to president_path(@booking.president)
+      redirect_to president_path(@booking.president), notice: 'Booking was successfully created.'
     else
       render 'presidents/show', status: :unprocessable_entity
     end
