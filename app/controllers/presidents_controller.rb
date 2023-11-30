7936b1856d26a9c4e684d6fcb50b721fe0b827a5
KKevin Falck <kevin.falck@gmail.com>
2023-11-30 12:25:22 +0100

    better home

--- app/controllers/presidents_controller.rb
+++ app/controllers/presidents_controller.rb
@@ -1,4 +1,5 @@
 class PresidentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @presidents = President.all
     # The `geocoded` scope filters only flats with coordinates
