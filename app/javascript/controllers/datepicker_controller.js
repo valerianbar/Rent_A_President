59976d43fe1569dc740179007ca1be2bc1420414
valerian barreau <valerianextra@gmail.com>
2023-11-30 18:01:44 +0300

    form adress input

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,5 +1,3 @@
-import { Controller } from "@hotwired/stimulus"
-
 // Connects to data-controller="datepicker"
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
